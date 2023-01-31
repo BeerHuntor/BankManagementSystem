672f940f99b58f0f12519895f3bf28738ab0cfdf
BeerHuntor <rt001a9138@gmail.com>
2023-01-31 14:38:07 +0000

    no change

--- src/net/HuntorGames/CheckingAccount.java
+++ src/net/HuntorGames/CheckingAccount.java
@@ -19,12 +19,13 @@ public class CheckingAccount{
             System.out.println("Insufficient Funds in your account!");
         } else {
             balance -= amountToWithdraw;
-            System.out.println("You have sucsessfully withdrawn " + "£" + amountToWithdraw +  ".  Your balance is now " + "£" + balance);
+            System.out.println("You have successfully withdrawn " + "£" + amountToWithdraw +  ".  Your balance is now " + "£" + balance);
         }
 
     }
     public void DepositMoney(float amountToDeposit)
     {
+        System.out.println("You have successfully deposited " + "£" + amountToDeposit +  ".  Your balance is now " + "£" + balance);
         balance += amountToDeposit;
     }
 
