cea5c1d4a40f48f6e60139beed19ec25ea399197
BeerHuntor <rt001a9138@gmail.com>
2023-01-31 14:36:47 +0000

    Added functionality to personal login system, allowing the customer to check balance, withdraw and deposit from their available balance.

--- src/net/HuntorGames/BankSystem.java
+++ src/net/HuntorGames/BankSystem.java
@@ -1,6 +1,7 @@
 package net.HuntorGames;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -21,6 +22,7 @@ public class BankSystem {
     private static Bank RequestedBank;
     private static int command;
     private static boolean IsLoggedIn = false;
+    private static boolean IsLoggedInPersonalAccount = false;
 
     public static void main(String[] args) {
         Dialog();
@@ -32,9 +34,15 @@ public class BankSystem {
             System.out.println("Welcome to the Banking System, What would you like to do?\nPress the corresponding number to navigate");
             System.out.println("1) Open Bank");
             System.out.println("2) Log into Bank");
-            System.out.println("3) Check personal bank balance");
-            command = scanner.nextInt(); // leaves /n in buffer.
-            scanner.nextLine(); //clears /n in buffer??
+            System.out.println("3) Log into personal bank account");
+
+            try {
+                command = scanner.nextInt(); // leaves /n in buffer.
+                scanner.nextLine(); //clears /n in buffer??
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input, please enter a valid number!");
+                scanner.nextLine();
+            }
             if (command == 1) { // open bank
                 OpenBankDialogue();
                 continue;
@@ -43,54 +51,100 @@ public class BankSystem {
                     BankLoginDialogue();
                 }
                 BankLoggedInDialogue();
-                //TODO: find a way to keep coming back to the logged in dialogue instead of resulting back at the main menu.
                 continue;
 
             } else if (command == 3) { // check personal account
                 String bankName = RequestInputString("What is the name of the bank that you bank with?");
-                if (BankExists(bankName)) {
+                if (GetBank(bankName) != null) {
                     String firstName = RequestInputString("What is your first name?");
                     String lastName = RequestInputString("What is your last name?");
                     int ssn = RequestInputInt("What is your Social Security Number?");
-
+                    IsLoggedInPersonalAccount = true;
+                    PersonalBankLoginDialogue(firstName, lastName, ssn);
                     //TODO: Finish implementing the funtionality of checking user accounts.
                 }
+            } else {
+                System.out.println("Invalid option, please try again!");
             }
             continue;
         }
     }
+    private static void PersonalBankLoginDialogue(String firstName, String lastName, int ssn)
+    {
+        CheckingAccount account = RequestedBank.GetCustomerAccount(firstName, lastName, ssn);
+        while (IsLoggedInPersonalAccount)
+        {
+            System.out.println("Welcome to your personal dashboard " + firstName + " " + lastName + "\nWhat would you like to do?\nPress the" +
+                            " corresponding number to navigate");
+            System.out.println("1) Check personal balance");
+            System.out.println("2) Deposit Money");
+            System.out.println("3) Withdraw Money");
+            System.out.println("4) Log out");
+
+            try {
+                command = scanner.nextInt();
+                scanner.nextLine();
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input, please press a number");
+                scanner.nextLine();
+            }
+            if(command == 1) {
+                System.out.println("Your available balance is £" + RequestedBank.GetCustomerAccount(firstName, lastName, ssn).getBalance());
+                continue;
+
+            } else if (command == 2) {
+                System.out.println("How much would you like to deposit?");
+                float amountToDeposit = scanner.nextFloat();
+                account.DepositMoney(amountToDeposit);
+                scanner.nextLine();
+                continue;
+            } else if (command == 3) {
+                System.out.println("How much would you like to withdraw?");
+                float amountToWithdraw = scanner.nextFloat();
+                account.WithdrawMoney(amountToWithdraw);
+                scanner.nextLine();
+
+            } else if (command == 4) {
+                IsLoggedInPersonalAccount = false;
+            }else{
+                System.out.println("Invalid option, please try again");
+            }
+        }
+    }
 
     private static void BankLoggedInDialogue() {
-        System.out.println("Welcome to the Banking System, You are logged in as" + RequestedBank.GetBankName() +
-                " What would you like to do?\nPress the corresponding number to navigate");
-        System.out.println("1) Register new customer");
-        System.out.println("2) See List Of Customers");
-        System.out.println("3) Return to main menu");
-        command = scanner.nextInt();
-        System.out.println(command);
-        scanner.nextLine();
-        System.out.println(command);
-        if (command == 1) { // register new customer
-            String firstName = RequestInputString("What is the first name of customer?");
-            String middleName = RequestInputString("What is the middle name of the customer?");
-            String lastName = RequestInputString("What is the last name of the customer?");
-            int age = RequestInputInt("What is the age of the customer?");
-            int ssn = RequestInputInt("What is the social security number of the customer?");
-            float depositAmount = RequestInputFloat("What is the initial deposit amount?");
-            RequestedBank.OpenCheckingAccount(RequestedBank.RegisterCustomer(firstName, middleName, lastName, age, ssn), depositAmount);
-            System.out.println("Checking account successful! Account created for " + firstName + " " + middleName + " " + lastName);
-            //TODO: return to bank login dialogue.
-        } else if (command == 2) { // return customer list
-            RequestedBank.GetCustomerList();
-        } else if (command == 3) { // return to main menu
-            IsLoggedIn = false;
-            return;
+        while (IsLoggedIn)
+        {
+            System.out.println("Welcome to the Banking System, You are logged in as" + RequestedBank.GetBankName() +
+                    " What would you like to do?\nPress the corresponding number to navigate");
+            System.out.println("1) Register new customer");
+            System.out.println("2) See List Of Customers");
+            System.out.println("3) Return to main menu");
+            command = scanner.nextInt();
+            scanner.nextLine();
+            if (command == 1) { // register new customer
+                String firstName = RequestInputString("What is the first name of customer?");
+                String middleName = RequestInputString("What is the middle name of the customer?");
+                String lastName = RequestInputString("What is the last name of the customer?");
+                int age = RequestInputInt("What is the age of the customer?");
+                int ssn = RequestInputInt("What is the social security number of the customer?");
+                float depositAmount = RequestInputFloat("What is the initial deposit amount?");
+                RequestedBank.OpenCheckingAccount(RequestedBank.RegisterCustomer(firstName, middleName, lastName, age, ssn), depositAmount);
+                System.out.println("Checking account successful! Account created for " + firstName + " " + middleName + " " + lastName);
+                continue;
+            } else if (command == 2) { // return customer list
+                RequestedBank.GetCustomerList();
+            } else if (command == 3) { // return to main menu
+                IsLoggedIn = false;
+                return;
+            }
+
         }
     }
 
     private static void BankLoginDialogue() {
         String bankName = RequestInputString("What is the name of the bank you would like to log into?");
-        if (BankExists(bankName)) {
+        if (GetBank(bankName) != null) {
             System.out.println("Bank Exists!");
             String managerName = RequestInputString("Please type the name of the bank manager");
             String managerPassword = RequestInputString("Thank you " + managerName + ", Please type the banks password!");
@@ -106,20 +160,17 @@ public class BankSystem {
         }
     }
 
-    //Checks the system to see if the bank requested to login exists.
-    private static boolean BankExists(String name) {
+    private static Bank GetBank(String bankName) {
         for (Bank bank : banks) {
-            if (bank.GetBankName().equalsIgnoreCase(name)) {
-                RequestedBank = GetBank(bank);
-                System.out.println(RequestedBank.GetBankName() + " " + RequestedBank.GetBankManagerName() + " " + RequestedBank.GetBankPassword());
-                return true;
+            if (bank.GetBankName().equalsIgnoreCase(bankName))
+            {
+                RequestedBank = bank;
+                if(IsLoggedIn) {
+                    System.out.println(RequestedBank.GetBankName() + " " + RequestedBank.GetBankManagerName() + " " + RequestedBank.GetBankPassword());
+                }
             }
         }
-        return false;
-    }
-
-    private static Bank GetBank(Bank bankToGet) {
-        return bankToGet;
+        return RequestedBank;
     }
 
     public static void OpenBankDialogue() {
@@ -141,7 +192,6 @@ public class BankSystem {
             return;
         }
         banks.add(new Bank(name, managerName, managerPassword));
-        System.out.println(banks.get(0).GetBankName());
     }
     
     public static String RequestInputString(String prompt){
